00edbfc8585b605a3b0ce5c4d29ed592fd5e9f1a
Florent Biville <f.biville@lateral-thoughts.com>
2013-04-17 20:55:24 +0200

    Very quick'n'dirty dashboard

---
+++ main.js
@@ -0,0 +1,11 @@
+// loader configuration
+require.config({
+    shim: {
+        'underscore': {
+            exports: '_'
+        },
+        'highcharts': {
+            exports: 'highcharts'
+        }
+    }
+});
